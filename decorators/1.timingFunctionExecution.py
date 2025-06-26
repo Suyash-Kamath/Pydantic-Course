a36abb6af7e238213c4a45fb07b827df6bda626d
Suyash Kamath <suyashkamath9@gmail.com>
2025-06-27 00:10:27 +0530

    New Concept: Decorators

---
+++ decorators/1.timingFunctionExecution.py
@@ -0,0 +1,20 @@
+# Write a decorator that measures the time a function takes to execute
+
+import time
+
+# Let's make this toll so that every function will go through this
+def timer(func):
+    def wrapper(*args,**kwargs):
+      start = time.time()
+      result =  func(*args,**kwargs)
+      end = time.time()
+      print(f"{func.__name__} ran in {end-start} time")
+      return result
+    return wrapper
+
+
+@timer # decorators hai , so whenever you call this function , then this won't get directly called , this function will go through the timer , return wrapper returns to the decorator , okay ?? , if you want to make decorator then make function inside function and inside that function runs/ gets executed
+def example_func(n):
+   time.sleep(n)
+
+example_func(2)
---
+++ decorators/2.debuggingFunctionCall.py
@@ -0,0 +1,27 @@
+# Create a Decorator to print the function name and the values of its arguments every time the function is called
+
+# Remember , formatted strings me None nahi aata hai
+
+def debug(func):
+    # Jitne bhi args aaye wo lelo , and jitne bhi key value pairs aayi hai wo bhi lelo
+    def wrapper(*args,**kargs):
+        # join method gives iterable list , the moment you use join() , you get iterable and you can use comprehension in join
+        args_value =', '.join(str(arg) for arg in args)
+        kwargs_value = ', '.join(f"${key}={value}" for key,value in kargs.items())
+        print(f"calling : {func.__name__} with args {args_value} and {kwargs_value} kwargs")
+        return func(*args,**kargs)
+
+        
+    return wrapper
+
+@debug
+def hello():
+    print("Hello")
+
+@debug    
+def greet(name,greeting="Hello"):
+    print(f"{greeting}, {name}")
+
+                # named parameter
+hello()
+greet("chai" , greeting="haanji ")
