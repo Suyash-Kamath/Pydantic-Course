c33496df899b0b534f44d04710049993a0099dfb
Suyash Kamath <suyashkamath9@gmail.com>
2025-06-26 12:16:53 +0530

    Module 3

--- 03-model-behaviour/example/chai-validator.py
+++ 03-model-behaviour/example/chai-validator.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel, field_validator, model_validator, computed_field # type: ignore
 
+# but dont we have controllers for that ?
 
 class User(BaseModel):
     username: str
-
+    # pehele run hotey hai pydantic ke jaane se pehele , mode is automatially before
     @field_validator('username')
     def username_length(cls, v):
         if len(v) < 4:
@@ -15,7 +16,9 @@ class SignupData(BaseModel):
     password: str
     confirm_password: str
 
+    # custom validation ke baad merepaas aao , iss decorator ke paas 
     @model_validator(mode='after')
+    # class, values in bracket
     def password_match(cls, values):
         if values.password != values.confirm_password:
             raise ValueError('Password do not match')
@@ -25,8 +28,9 @@ class Product(BaseModel):
     price: float
     quantity: int
 
-
+    # naya property banake aap easily access karke validation bhi kar saktey ho 
     @computed_field
     @property
     def total_price(self) -> float:
         return self.price * self.quantity
+    # full name banana hai toh first name and last name lelo , aise bhi property ban sakti hai
