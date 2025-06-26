e5af05e94e695c3e50474ee1e672802bdc3b4832
Suyash Kamath <suyashkamath9@gmail.com>
2025-06-27 00:24:16 +0530

    New Concept: Decorators

---
+++ decorators/3.cacheReturnValues.py
@@ -0,0 +1,23 @@
+# Implement a Decorator that caches the return values of a function, so that when it's called with the same argument, the cached value is returned instead of re-executing the function
+import time
+
+def cache(func):
+    cache_value = {} # dict because accessing is very compared to array because indexing ka dhyan rakhna possible nahi , so args basis pe uska key banaye and access the value
+    print(cache_value)
+    def wrapper(*args,**kwargs):
+        if args in cache_value:
+            return cache_value[args]
+        result=func(*args)
+        cache_value[args] = result
+        return result
+    
+    return wrapper
+
+
+@cache
+def long_running_function(a,b):
+    time.sleep(4) # let's say database call 
+    return a+b
+
+print(long_running_function(2,3))
+print(long_running_function(2,3))
